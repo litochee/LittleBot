f3baa11e6802d92134a9fcbd3e66899773d70d30
Tyler <litochee@icloud.com>
2017-07-21 19:12:07 -0400

    redeem points now work.
    redeem points work, and did small update to say to not spam console.

--- commands/redeem.js
+++ commands/redeem.js
@@ -6,15 +6,15 @@ exports.run = (client, message, args, sql) =>{
         if(!eRedeem){
             message.reply("Please enter a valid event point code.");
         }else{
-            sql.get(`SELECT * FROM eventCodes WHERE guildID='${message.guild.id} AND coupon='${eRedeem}'`).then(crow =>{
+            sql.get(`SELECT * FROM eventCodes WHERE guildID='${message.guild.id}' AND coupon='${eRedeem}'`).then(crow =>{
                 if(!crow){
                     message.reply(`${eRedeem} is not a valid code.`);
                 }else{
                     if(crow.status == "Available"){
                         let eDate = moment().format('LLLL');
                         let tAmount = iUser.eventPoints + crow.amount;
-                        sql.run(`UPDATE eventCodes SET redeemedBy='${message.author.username}', status='Used',date=${eDate} WHERE guildID='${message.guild.id} AND coupon='${eRedeem}'`);
-                        sql.run(`UPDATE userScores SET eventPoints=${tAmount} WHERE guildID='${message.guild.id} AND userID='${message.author.id}'`);
+                        sql.run(`UPDATE eventCodes SET redeemedBy='${message.author.username}', status='Used', date='${eDate}' WHERE guildID='${message.guild.id}' AND coupon='${eRedeem}'`);
+                        sql.run(`UPDATE userScores SET eventPoints=${tAmount} WHERE guildID='${message.guild.id}' AND userID='${message.author.id}'`);
                         message.reply(`CONGRATS! You have redeemed ${eRedeem} for ${crow.amount} event points!`);
                     }else{
                         message.reply(`Sorry this has already been redemeed. Have a great day!`);
--- commands/say.js
+++ commands/say.js
@@ -2,10 +2,10 @@ exports.run = (client, message, args) =>{
 
   message.channel.send(args.join(" ")); //joins it back together
   const channel = message.guild.channels.find('name', 'bot_logs'); //checks for bot logs
+  message.delete();
   if (!channel) { //if it doesn't find channel
-    console.log(`${message.author.username} said: ${message.content}`); //console log
+    retun;
   }else{
     channel.send(`${message.author.username} said: ${message.content}`);
   }
-    message.delete();
 }
